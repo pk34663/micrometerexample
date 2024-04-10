efac2e7c3c32b08fdf5aaca1ba1ddb14a389569f
Phil Kirk <pk34663@gmail.com>
2024-04-10 09:49:29 +0100

    refactor

--- src/main/java/org/example/Runner.java
+++ src/main/java/org/example/Runner.java
@@ -8,21 +8,23 @@ import static org.example.App.prometheusMeterRegistry;
 import static org.example.App.requests;
 
 public class Runner {
+    private int counter = 0;
     public Runner() {}
 
-    public static void randomDelay() throws InterruptedException
+    public void randomDelay() throws InterruptedException
     {
         Timer.Sample sample = Timer.start(prometheusMeterRegistry);
         Random random = new Random();
         Thread.sleep(random.nextInt(1001));
         sample.stop(prometheusMeterRegistry.timer("test.timer"));
     }
-    public static void run() throws InterruptedException
+    public void run() throws InterruptedException
     {
         while(true)
         {
             requests.increment();
             randomDelay();
+            counter++;
         }
     }
 }
