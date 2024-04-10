4b80046c33c3cd7567d9806bfb85bf27a0a95d3b
Phil Kirk <pk34663@gmail.com>
2024-04-10 19:05:38 +0100

    refactor

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,27 +1,18 @@
 package org.example;
 
 import com.sun.net.httpserver.HttpServer;
-import io.micrometer.core.annotation.Timed;
-import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Metrics;
 import io.micrometer.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 public class App 
 {
     public static PrometheusMeterRegistry prometheusMeterRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
-    public static Counter requests = prometheusMeterRegistry.counter("requests");
-    public static Timer timer = Timer
-            .builder("test.timer")
-            .publishPercentiles(0.3,0.5,0.95)
-            .publishPercentileHistogram()
-            .register(prometheusMeterRegistry);
 
     public static void initEndPoint()
     {
@@ -43,7 +34,12 @@ public class App
 
     public static void main( String[] args ) throws InterruptedException
     {
+        Metrics.globalRegistry.add(prometheusMeterRegistry);
         initEndPoint();
+        for (MeterRegistry registry : Metrics.globalRegistry.getRegistries()) {
+            System.out.println(registry.toString());
+        }
+
         Runner runner = new Runner();
         runner.run();
     }
--- src/main/java/org/example/Runner.java
+++ src/main/java/org/example/Runner.java
@@ -1,22 +1,36 @@
 package org.example;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.Timer;
 
+import java.time.Duration;
 import java.util.Random;
 
 import static org.example.App.prometheusMeterRegistry;
-import static org.example.App.requests;
 
 public class Runner {
     private int counter = 0;
+    private final Counter requests = Counter
+                                        .builder("requests")
+                                        .description("number of requests")
+                                        .register(Metrics.globalRegistry);
+    private final static Timer timer = Timer
+            .builder("test.timer")
+            .publishPercentiles(0.3,0.5,0.95)
+            .publishPercentileHistogram()
+            .serviceLevelObjectives(Duration.ofMillis(250),Duration.ofMillis(500),Duration.ofMillis(750),Duration.ofMillis(800))
+            .minimumExpectedValue(Duration.ofMillis(1))
+            .maximumExpectedValue(Duration.ofSeconds(10))
+            .register(Metrics.globalRegistry);
     public Runner() {}
 
     public void randomDelay() throws InterruptedException
     {
-        Timer.Sample sample = Timer.start(prometheusMeterRegistry);
+        Timer.Sample sample = Timer.start(Metrics.globalRegistry);
         Random random = new Random();
         Thread.sleep(random.nextInt(1001));
-        sample.stop(prometheusMeterRegistry.timer("test.timer"));
+        sample.stop(Metrics.globalRegistry.timer("test.timer"));
     }
     public void run() throws InterruptedException
     {
